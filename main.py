e431cc25c8624596f5541c8b54041b7d9e61a870
Bob Jackson <wangyousong@9191offer.com>
2023-07-09 17:18:50 +0800

    add code for chapter 1

---
+++ main.py
@@ -0,0 +1,12 @@
+# The Fibonacci sequence
+# 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
